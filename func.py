d44c536a12a58264f5f20906c98f4ebb9f9290c9
Lin <linwen.linkedin@gmail.com>
2025-03-29 13:54:18 -0400

    update

--- func.py
+++ func.py
@@ -145,7 +145,6 @@ def generate_string_from_parsed_regex(parsed_pattern: sre_parse.SubPattern, posi
         op, args = token
 
         if op == sre_parse.LITERAL:
-            print("sre_parse.LITERAL")
             return chr(args), current_position_idx  # LITERAL = Character literal
 
         elif op == sre_parse.MAX_REPEAT:
@@ -165,13 +164,9 @@ def generate_string_from_parsed_regex(parsed_pattern: sre_parse.SubPattern, posi
             return result, current_position_idx
 
         elif op == sre_parse.SUBPATTERN:
-            # Handle cases where args has more than 3 elements
             for subpattern in args:
                 if isinstance(subpattern, sre_parse.SubPattern):
                     return generate_string_from_parsed_regex(subpattern, position_accept_chars, position, current_position_idx)
-                else:
-                    #print(type(subpattern))
-                    pass
 
         elif op == sre_parse.BRANCH:  # OR operator
             _, alternatives = args
@@ -244,8 +239,14 @@ def generate_string_from_parsed_regex(parsed_pattern: sre_parse.SubPattern, posi
             # ^, $, \A, \Z, \b, \B (Position assertions) - Skip these
             return "", current_position_idx
 
-        elif op in [sre_parse.CATEGORY, sre_parse.CATEGORY_DIGIT, sre_parse.CATEGORY_SPACE, sre_parse.CATEGORY_WORD, sre_parse.CATEGORY_NOT_WORD]:  # Character categories (\d, \w, \s)
-            print("sre_parse.CATEGORY")
+        elif op == sre_parse.ASSERT_NOT:
+            return "", current_position_idx #Unsupported category
+
+        elif op == sre_parse.ASSERT:
+            print(f"Unsupported token: {op}")
+            return "", current_position_idx #Unsupported category
+
+        elif op in [sre_parse.CATEGORY]:
             category = args
             if category == sre_parse.CATEGORY_DIGIT:
                 return random.choice(string.digits), current_position_idx
@@ -255,11 +256,8 @@ def generate_string_from_parsed_regex(parsed_pattern: sre_parse.SubPattern, posi
                 return ' ', current_position_idx
             elif category == sre_parse.CATEGORY_NOT_WORD:
                 return random.choice("!#%&',-/:;<=>@`~"), current_position_idx #Placeholder until better logic
-
             else:
                 return "", current_position_idx #Unsupported category
-
-
         else:
             print(f"Unsupported token: {op}")
             return "", current_position_idx #Ignore unhandled token
@@ -306,27 +304,41 @@ if __name__ == '__main__':
     pattern_string = r"(a|b)*c[12345]{4}\w{3}"
     pattern_string = r"(_en|_fr){,3}"
     pattern_string = r"^/(?=.{6,20}$)\D*\d/"
-    pattern_string = r"[0-5]{2,5}(\d{3,})"
     pattern_string = r"[qwrtpsdfghjklzxcvbnm]*\w*(\d{2,})\W*"
-
+    pattern_string = r"\((?!2001)[0-9a-zA-z _\.\-:]*\)"
+    pattern_string = r"^(feature|bugfix|hotfix|release)/[a-z0-9-]+(-\d+)?$"
+    pattern_string = r"/\d+(?!\s*(kg|lb))/"
+    pattern_string = r"[0-5]{2,5}(\d{3,})"
+    pattern_string = r"^(feature|bugfix|hotfix|release)/[a-z0-9-]+(-\d+)?$"
+    pattern_string = r"^[MJHDWTRPCN]{1}[aeiou]{1,2}[qsghjklxz]{1}[aeiouy]{1}[bhjdksapel]{1}[rstuvwxyz]+$"
 
     parsed_pattern = sre_parse.parse(pattern_string)
-    #is_valid_manual = verify_generated_string(pattern_string, "gwmbwqd100WhHHhBZ43699675@~/,")
-    #print(f"Is valid manual: {is_valid_manual}")
+    # is_valid_manual = verify_generated_string(pattern_string, "100 dollars")
+    # print(f"Is valid manual: {is_valid_manual}")
 
     print(f"Regex pattern: {pattern_string}")
     print(f"parsed_pattern: {parsed_pattern}")
     #print(sorted(list("qwrtpsdfghjklzxcvbnm")))
-    for p in range(100, 110):
-        position_accept_chars = get_all_accept_word_from_regex(parsed_pattern)
-        #print(f"position_accept_chars: {position_accept_chars}")
 
-        # generated_string, _ = generate_string_from_parsed_regex(parsed_pattern=parsed_pattern)
-        # print(f"Generated string: {generated_string}")
-
+    position_accept_chars = get_all_accept_word_from_regex(parsed_pattern)
+    print(f"position_accept_chars: {position_accept_chars}")
+    r=[]
+    rr=set()
+    v=0
+    for p in range(10000000, 10100000):
         generated_string, _ = generate_string_from_parsed_regex(parsed_pattern=parsed_pattern, position_accept_chars=position_accept_chars, position=p, current_position_idx=0)
         is_valid = verify_generated_string(pattern_string, generated_string.replace("|",""))
+        r.append(generated_string)
+        rr.add(generated_string)
+        if is_valid:
+            v+=1
 
-        print(f"Generated string: {generated_string}")
-        #print(f"Is valid: {is_valid} {is_valid_manual}")
-        print(f"Is valid: {is_valid}")
+    print("--------------------------------------------------------------------------")
+    print(f"verified count[{v}] total generated[{len(r)}] unique generated[{len(rr)}]")
+    print("--------------------------------------------------------------------------")
+    print(f"{r[1]}\n{r[100]}\n{r[200]}\n{r[500]}\n{r[1000]}\n{r[1500]}\n{r[1999]}\n")
+    print("--------------------------------------------------------------------------")
+
+    for _r in r[:10]:
+        print(_r)
+    print("--------------------------------------------------------------------------")
